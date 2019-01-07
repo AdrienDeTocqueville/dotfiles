1564928247417eb91cb3d5dcf7ccbfb8c73fa0d4
Adrien de Tocqueville <adrien.detocqueville@gmail.com>
2019-01-07 22:12:56 +0100

    install script

--- more.sh
+++
@@ -1,8 +0,0 @@
-# Useful packages
-sudo apt-get install clang
-sudo apt-get install nodejs npm
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install man-db
-sudo apt-get install manpages-dev
-sudo apt-get install glibc-doc
