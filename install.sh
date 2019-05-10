6824406957827ebff66810dfb24bfeb64ad560e4
Adrien de Tocqueville <adrien.detocqueville@gmail.com>
2019-05-11 00:03:09 +0200

    scripts

--- install.sh
+++ install.sh
@@ -1,32 +1,6 @@
-read -p "Install essentials ? " yn
-case $yn in
-	[Yy]* )
-	sudo apt-get install build-essential make git screen
-	;;
-esac
+sudo apt-get install vim
+sudo apt-get install build-essential make git tmux
+#sudo apt-get install nodejs npm
 
-read -p "Install vim ? " yn
-case $yn in
-[Yy]* )
-	sudo apt-get install vim
-
-	cat >> ~/.vimrc < vimrc
-	;;
-esac
-
-read -p "Install Node ? " yn
-case $yn in
-	[Yy]* )
-	sudo apt-get install nodejs npm
-	;;
-esac
-
-read -p "Install zsh ? " yn
-case $yn in
-	[Yy]* )
-	sudo apt-get install zsh curl
-	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
-
-	cat >> ~/.zshrc < zshrc
-	;;
-esac
+sudo apt-get install zsh curl
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
---
+++ make_ln.sh
@@ -0,0 +1,7 @@
+ ln -s ~/config/zshrc ~/.zshrc
+ ln -s ~/config/vimrc ~/.vimrc
+ ln -s ~/config/gdbinit ~/.gdbinit
+ ln -s ~/config/screenrc ~/.screenrc
+ ln -s ~/config/tmux.conf ~/.tmux.conf
+ ln -s ~/config/gitconfig ~/.gitconfig
+ ln -s ~/config/elinks.conf ~/.elinks.conf
