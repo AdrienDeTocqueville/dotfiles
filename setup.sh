883fa12e2b6fa642b6cde7cd87c950b89af5f202
Adrien de Tocqueville <adrien.detocqueville@gmail.com>
2019-09-15 22:31:06 +0000

    Added various configs

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+# root config
+#cp custom.map /usr/share/kbd/keymaps/
+#cat KEYMAP=custom > /etc/vconsole.conf
+#cp xorg.conf /etc/X11/xorg.conf
+
+# standard config
 ln -s ~/config/zshrc ~/.zshrc
 ln -s ~/config/vimrc ~/.vimrc
 ln -s ~/config/vim ~/.vim
@@ -5,9 +11,12 @@ ln -s ~/config/gdbinit ~/.gdbinit
 ln -s ~/config/screenrc ~/.screenrc
 ln -s ~/config/tmux.conf ~/.tmux.conf
 ln -s ~/config/gitconfig ~/.gitconfig
+ln -s ~/config/elinks ~/.elinks
 
+# standard .config
+mkdir -p ~/.config
+ln -s ~/config/i3 ~/.config/i3
+
+# X config
 ln -s ~/config/xinitrc ~/.xinitrc
 ln -s ~/config/Xmodmap ~/.Xmodmap
-
-ln -s ~/config/elinks ~/.elinks
-ln -s ~/config/i3 ~/.config/i3
