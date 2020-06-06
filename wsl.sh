8dcf6d1a4ecdaa168dc9266edf466784559d407f
Adrien de Tocqueville <adrien.detocqueville@gmail.com>
2020-06-06 19:42:46 +0200

    fix wsl script

--- wsl.sh
+++ wsl.sh
@@ -1,23 +1,36 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install curl git zsh ctags unzip
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
+sudo apt install -y curl git zsh ctags unzip
 
+# Oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
+rm ~/.zshrc
+
+# Neovim
 mkdir ~/nvim
 curl -fLo ~/nvim/nvim.appimage https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod u+x ~/nvim/nvim.appimage
 ~/nvim/nvim.appimage --appimage-extract
+mv squashfs-root/* ~/nvim
+rm -rf squashfs-root
 
+### Plugin manager
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-curl -fLo ~/nvim/yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x86.zip
-unzip ~/nvim/yank.zip
-mv ~/nvim/win32yank.exe /mnt/d/Softwares/win32yank.exe
+### Clipboard
+mkdir ~/win32yank
+curl -fLo ~/win32yank/yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x86.zip
+unzip ~/win32yank/yank.zip -d ~/win32yank
+mv ~/win32yank/win32yank.exe /mnt/d/Softwares/win32yank.exe
+rm -rf ~/win32yank
 
-rm ~/.zshrc
+# Config files
 ~/dotfiles/setup.sh
 
-echo 'alias vim=~/nvim/squashfs-root/usr/bin/nvim'	>> ~/.extrc
-echo 'alias dl="/mnt/d/Downloads/"'			>> ~/.extrc
-echo 'alias prgm="/mnt/d/Programs/"'			>> ~/.extrc
-echo 'export PATH="$PATH:/mnt/d/Softwares"'		>> ~/.extrc
+# WSL specific aliases
+echo 'alias vim=~/nvim/usr/bin/nvim'		>> ~/.extrc
+echo 'alias dl="/mnt/d/Downloads/"'		>> ~/.extrc
+echo 'alias prgm="/mnt/d/Programs/"'		>> ~/.extrc
+echo 'export PATH="$PATH:/mnt/d/Softwares"'	>> ~/.extrc
+
+exec zsh
