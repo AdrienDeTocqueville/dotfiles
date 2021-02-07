0e34aeb736b768fdf285a1d238f8159daeba3736
Adrien de Tocqueville <adrien.detocqueville@gmail.com>
2021-02-07 01:30:35 +0100

    fix file

--- wsl.sh
+++ wsl.sh
@@ -6,7 +6,7 @@ else
 BINDIR="/mnt/c/Softwares"
 fi
 
-ln -s $BINDIR/Git/bin/git.exe  /bin/git
+sudo ln -s $BINDIR/Git/bin/git.exe  /bin/git
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y curl zsh ctags unzip silversearcher-ag
